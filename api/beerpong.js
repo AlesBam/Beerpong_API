536f1e12bd8de94a6e225b65fb768a07c24eef3c
Alda <alesbambas@gmail.com>
2025-05-14 23:56:35 +0200

    api setup

--- api/beerpong.js
+++ api/beerpong.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     try {
-      const response = await fetch('https://script.google.com/macros/s/AKfycbzTI-AEr8CPtW3jQSEt_jAoKnvmuA43nuRyTWwRGeq226ZPNSRfIu9HokDHqJeOkwBh/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbyOjdECOOtLNhkWls_1OYQJooNHNtQpt1IKzUKsU4wqZWeIt4qer67EaatnaD2I8ZzT/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body)
